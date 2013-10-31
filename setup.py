7ad80c959fedf7efb9db7c7168a200e5de8c2ff9
Tsuname Tendou <tsuname@gmail.com>
2013-10-30 23:55:26 -0700

    Reformatted everything for setuptools use
    * Renamed src to reeffit and made it a module

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='REEFFIT',
+        version='0.5',
+        description='RNA Ensemble Extraction From Footprinting Insights Technique',
+        author='Pablo Cordero',
+        author_email='tsuname@stanford.edu',
+        url='http://rmdb.stanford.edu/reeffit',
+        packages=['reeffit'],
+        install_requires=['numpy', 'scipy', 'joblib', 'pymc', 'mdp', 'matplotlib', 'cvxopt']
+        )

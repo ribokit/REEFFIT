3f85f0994275b80ede493d0d7a4a7f454021237a
Pablo Cordero <dimenwarper@gmail.com>
2013-12-13 11:39:53 +0000

    Added structure grid functionality to report generation

--- reeffit/generate_report.py
+++ reeffit/generate_report.py
@@ -13,119 +13,112 @@ import pickle
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('benchmarkfile', type=argparse.FileType('r'))
+parser.add_argument('resultdir', type=argparse.FileType('r'))
+parser.add_argument('name', type=str)
 parser.add_argument('prefix', type=str)
 
 args = parser.parse_args()
 
-complement = {'A':'U', 'C':'G', 'U':'A', 'G':'C'}
+result_dir = args.result_dir + '/'
+template_dir = os.environ['REEFFIT_HOME'] + '/svg_templates/'
 
-BENCHMARKDIR = 'insilico_benchmark/'
+def fill_in_template(data_dict, templatefile, svgfile):
+    for line in templatefile.readlines():
+        newline = line
+        for tag, value in data_dict.iteritems():
+            newline = newline.replace('{%s}' % tag, value)
+        svgfile.write(newline)
+    return svgfile
 
-def find_cluster(clusters, structure):
-    minscore = inf
-    bestclust = None
-    for k, stlist in clusters.iteritems():
-        for st in stlist:
-            score = 0
-            for i in xrange(len(st)):
-                if st[i] != structure[i]:
-                    score += 1
-            if score < minscore:
-                minscore = score
-                bestclust = k
-    return bestclust
-templatefname = 'svg_templates/insilico_comparison.svg'
+def get_attributes(line):
+    att = ''
+    attributes = []
+    for s in line:
+        if read:
+            att += s
+        if s == '"' and not read:
+            read = True
+        if s == '"' and read:
+            read = False
+            attributes.append(att)
+            att = ''
+    return attributes
+
+def generate_structure_grid(structure_files, titles):
+    svgstrings = ['']*len(structure_files)
+    bounding_boxes = []
+    for i, sfile in enumerate(structure_files):
+        read = False
+        bounding_box = [0,0]
+        for line in sfile.readlines():
+            if 'line' in line and not read:
+                read = True
+            if read:
+                attributes =  get_attributes(line)
+                for att in attributes:
+                    try:
+                        val = float(att)
+                        if val < bounding_box[0]:
+                            bounding_box[0] = val
+                        if val > bounding_box[1]:
+                            bounding_box[1] = val
+                    except ValueError:
+                        pass
+                if '</svg>' in line:
+                    break
+                svgstrings[i] += line
+        bounding_boxes.append(bounding_box)
+    xoffset = 0
+    yoffset = 0
+    max_bounding_x = 0
+    j = 0
+    gridstr = ''
+    max_bounding_x = 0
+    for i, svgs in enumerate(svgstrings):
+        if j > 3:
+            j = 0
+            xoffset += max_bounding_x
+            yoffset = 0
+            max_bounding_x = 0
+        title = '<text x="%s" y="%s" text-anchor="middle" font-family="Verdana"' % (xoffset, yoffset)
+        title += 'font-size="12" fill="rgb(25%%, 25%%, 25%%)">%s</text>\n' % titles[i]
+        yoffset += 15
+        group_head = '<g\n   transform="translate(%s, %s)">\n' % (xoffset, yoffset)
+        gridstr += title + group_head + svgs + '</g>\n'
+        yoffset += bounding_boxes[i][1]
+        max_bounding_x = max(max_bounding_x, bounding_boxes[i][0])
+        j += 1
+    return gridstr
+    
+
+# Structure plot
+structure_files = []
+titles = []
+for fname in os.listdir(result_dir):
+    if '%s_structure' % args.prefix in fname and '.svg' in fname:
+        structure_files.append(open(result_dir + fname))
+        idx = fname.replace('%s_structure', '').replace('.svg','')
+        titles.append(args.name + idx)
+data_dict['structure_grid'] = generate_structure_grid(structure_files, titles)
+svgfile = fill_in_template(data_dict, template_dir + 'structures.svg', open('%s%s_structure_page.svg' % (result_dir, args.prefix)))
+svgfile.close()
+
+
+"""
 currdir = os.getcwd()
-for line in args.benchmarkfile.readlines():
-    if line.strip()[0] == '#':
-        continue
-    else:
-        name, sequence, structures = line.strip().split('\t')
-        structures = structures.split(',')
-        outname = name.lower().replace(' ','_').replace(';','_').replace('/','_')
-        pathname = BENCHMARKDIR + outname
-        result_dir = '%s/%s_results/' % (BENCHMARKDIR, args.prefix)
-        result_prefix = '%s/%s/%s_' % (BENCHMARKDIR, outname, args.prefix)
-        select_structs =  [l.strip() for l in open('%sstructure_medoids.txt' % result_prefix).readlines()]
-        print 'Doing %s' % outname
 
-        select_struct_vecs = []
-        for s in select_structs:
-            select_struct_vecs.append([1 if x == '.' else 0 for x in s])
-        select_struct_vecs = array(select_struct_vecs)
+        newline = line.replace('{exportdir}', result_dir).replace('{name}', outname).replace('{figdir}', '').replace('{id}', outname)
+print 'Making SVG file for %s' % outname
+templatefile = open(templatefname)
+svgfile = open('%s/%s_paper_figure.svg' % (result_dir, outname), 'w')
+svgfile.close()
+templatefile.close()
 
-        
-        clusters =  defaultdict(list)
-        cluster_indices =  defaultdict(list)
-        all_structs = []
-        all_struct_vecs = []
-        cluster_colors = {}
-        cluster_ordering = []
-        for l in open('%s/%s/structure_clusters.txt' % (BENCHMARKDIR, outname)).readlines():
-            k, v = l.strip().split('\t')[:2]
-            cluster_colors[k] = (205./255, 201./255, 201./255, 1.0)
-            cluster_ordering.append(k)
-            clusters[k].append(v)
-            all_structs.append(v)
-            all_struct_vecs.append([1 if s == '.' else 0 for s in v])
-        all_struct_vecs = array(all_struct_vecs)
-
-        energies = get_free_energy_matrix(all_structs, [sequence])
-        struct_weights = calculate_weights(energies)[0]
-
-        assignments = {}
-        true_struct_vecs = []
-        for i, s in enumerate(structures):
-            clust = find_cluster(clusters, s)
-            assignments[s] = clust
-            true_struct_vecs.append([1 if x == '.' else 0 for x in s])
-        true_struct_vecs = array(true_struct_vecs)
-
-
-        select_assignments = {}
-        for i, s in enumerate(select_structs):
-            select_assignments[s] = clust
-
-        if len(structures) > len(select_structs):
-            for i, s in enumerate(select_structs):
-                clust = find_cluster(clusters, s)
-                cluster_colors[clust] = STRUCTURE_COLORS[i]
-        else:
-            for i, s in enumerate(structures):
-                clust = find_cluster(clusters, s)
-                cluster_colors[clust] = STRUCTURE_COLORS[i]
-
-        print 'Making PCA plot for %s' % outname 
-        U, s, Vh = svd(all_struct_vecs.T)
-        basis = U[:,:2]
-        all_struct_coordinates = dot(all_struct_vecs, basis)
-        true_struct_coordinates = dot(true_struct_vecs, basis)
-        select_struct_coordinates = dot(select_struct_vecs, basis)
-
-        figure(1)
-        clf()
-
-        all_sizes = [max(50, struct_weights[i]*50000) for i in range(len(struct_weights))]
-        scatter(all_struct_coordinates[:,0], all_struct_coordinates[:,1], c=[cluster_colors[c] for c in cluster_ordering], alpha=0.6, linewidth=1, s=all_sizes)
-        scatter(true_struct_coordinates[:,0], true_struct_coordinates[:,1], c=[cluster_colors[assignments[s]] for s in structures], linewidth=1.5, marker='*', s=500)
-        scatter(select_struct_coordinates[:,0], select_struct_coordinates[:,1], c=[cluster_colors[select_assignments[s]] for s in select_structs], linewidth=1.5, marker='D', s=100)
-        savefig('%s/%s_pca_cluster_plot.png' % (result_dir, outname), dpi=300)
-
-        print 'Making SVG file for %s' % outname
-        templatefile = open(templatefname)
-        svgfile = open('%s/%s_paper_figure.svg' % (result_dir, outname), 'w')
-        for line in templatefile.readlines():
-            newline = line.replace('{exportdir}', result_dir).replace('{name}', outname).replace('{figdir}', '').replace('{id}', outname)
-            svgfile.write(newline)
-        svgfile.close()
-        templatefile.close()
-
-        print 'Exporting to PDF'
-        os.chdir(result_dir)
-        os.system('inkscape -z -D --file=%s_paper_figure.svg --export-pdf=%s_paper_figure.pdf --export-area-drawing  --export-text-to-path' % (outname, outname))
-        os.chdir(currdir)
-
+print 'Exporting to PDF'
+os.chdir(result_dir)
+os.system('inkscape -z -D --file=%s_paper_figure.svg --export-pdf=%s_paper_figure.pdf --export-area-drawing  --export-text-to-path' % (outname, outname))
+os.chdir(currdir)
+"""
 print 'Done'
 
 

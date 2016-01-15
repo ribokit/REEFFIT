12c1ceec9a0261549212ad7e0350d8d16cbeb5d7
T47 <t47@stanford.edu>
2016-01-15 10:40:00 -0800

    change url to https

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(name='REEFFIT',
         description='RNA Ensemble Extraction From Footprinting Insights Technique',
         author='Pablo Cordero',
         author_email='tsuname@stanford.edu',
-        url='http://rmdb.stanford.edu/reeffit',
+        url='https://rmdb.stanford.edu/reeffit',
         packages=['reeffit'],
         install_requires=['numpy', 'scipy', 'joblib', 'pymc', 'matplotlib', 'cvxopt']
         )
